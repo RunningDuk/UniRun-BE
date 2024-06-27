c8219448e1e11207841f4982fc02f32c70381521
JIMIN HWANG <hwang0010@naver.com>
2024-06-28 01:16:50 +0900

    first commit

---
+++ src/main/java/com/runningduk/unirun/UnirunApplication.java
@@ -0,0 +1,13 @@
+package com.runningduk.unirun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnirunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnirunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runningduk/unirun/UnirunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runningduk.unirun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnirunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
