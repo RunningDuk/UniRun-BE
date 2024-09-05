d2fb4e4db84adb089dd181951bdd327e08f8e009
JIMIN HWANG <hwang0010@naver.com>
2024-09-05 15:29:37 +0900

    refactor: API 공통 응답 형식 생성
    - statusCode, message, data, sendTime을 포함한 공통 응답 형식 생성합니다.

---
+++ src/main/java/com/runningduk/unirun/api/response/CommonApiResponse.java
@@ -0,0 +1,24 @@
+package com.runningduk.unirun.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class CommonApiResponse<T> {
+    private int statusCode;
+    private final String message;
+    private final T data;
+    private final ZonedDateTime sendTime = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+
+    public static ResponseEntity<CommonApiResponse> toEntity(final HttpStatus httpStatus) {
+        return ResponseEntity.status(httpStatus).body(null);
+    }
+}
